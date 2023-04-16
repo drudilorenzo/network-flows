776af6b14fd49acf397e6871dc30e4f0ff699917
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-04-16 23:47:52 +0200

    feat(utils): create residual graph.

--- src/utils/GraphUtils.cpp
+++ src/utils/GraphUtils.cpp
@@ -8,7 +8,7 @@
 #include "data_structures/graph/Edge.h"
 
 namespace utils {
-    data_structures::Graph GraphUtils::createGraphFromFile(string file_name) {
+    data_structures::Graph GraphUtils::CreateGraphFromFile(string file_name) {
         ifstream infile (file_name);
 
         if (infile) {
@@ -41,6 +41,28 @@ namespace utils {
         } else {
             throw std::invalid_argument("File not found");
         }
+    }
 
+    data_structures::Graph GraphUtils::GetResidualGraph(data_structures::Graph graph) {
+        data_structures::Graph residual_graph = data_structures::Graph(graph.GetNumNodes());
+
+        for (int u = 0; u < graph.GetNumNodes(); u++) {
+            for (auto e : graph.GetNodeAdjList(u)) {
+                int head = e.GetHead();
+                int tail = e.GetTail();
+                int ca = e.GetCapacity();
+                int cs = e.GetCost();
+
+                // Add the edge to the residual graph
+                data_structures::Edge edge = data_structures::Edge(head, tail, ca, cs);
+                residual_graph.AddEdge(edge);
+
+                // Add the reverse edge to the residual graph
+                data_structures::Edge rev_edge = data_structures::Edge(tail, head, 0, -cs);
+                residual_graph.AddEdge(rev_edge);
+            }
+        }
+
+        return residual_graph;
     }
 }
--- src/utils/GraphUtils.h
+++ src/utils/GraphUtils.h
@@ -18,8 +18,20 @@ namespace  utils {
              *
              * @param file_name name of the file to read
              * @return (data_structure::Graph) graph created from the file inputs
+             * @throws invalid_argument if the file does not exist
+             * @throws invalid_argument if the file is not formatted correctly
              */
-            static data_structures::Graph createGraphFromFile(string file_name);
+            static data_structures::Graph CreateGraphFromFile(string file_name);
+
+            /**
+             * Get the residual graph of the given graph.
+             *
+             * (see https://www.hackerearth.com/practice/algorithms/graphs/maximum-flow/tutorial/)
+             *
+             * @param graph the graph to get the residual graph from
+             * @return (data_structure::Graph) the residual graph
+             */
+            static data_structures::Graph GetResidualGraph(data_structures::Graph graph);
     };
 }
 
