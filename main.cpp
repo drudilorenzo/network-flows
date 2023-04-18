1eabb82d190f1468801f74d2a5ffe296036020b1
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-04-18 23:42:41 +0200

    feat(chore): add menu.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 #include "data_structures/graph/Graph.h"
 #include "utils/GraphUtils.h"
@@ -15,14 +20,42 @@ int main(int argc, char **argv) {
 
     // Read graph from file
     try {
-        std::shared_ptr<data_structures::Graph> graph = utils::GraphUtils::CreateGraphFromJSON(filename);
-        graph->PrintGraph();
+        auto graph = utils::GraphUtils::CreateGraphFromJSON(filename);
 
-        int source = 0;
-        int sink = 5;
-        int max_flow = algorithms::MaximumFlowAlgorithms::EdmondsKarp(graph, source, sink);
+        std::cout << endl;
+        std::cout << "What do you want to do?" << endl;
+        std::cout << "1. Maximum flow" << endl;
+        std::cout << "2. Minimum cost flow" << endl;
+        std::cout << "3. Exit" << endl;
+        std::cout << "Enter your choice: ";
+        int choice;
+        std::cin >> choice;
+        std::cout << endl;
 
-        std::cout << "Max flow: " << max_flow << std::endl;
+        switch (choice) {
+            case 1: {
+                int source = 0;
+                int sink = 5;
+                auto result = algorithms::MaximumFlowAlgorithms::EdmondsKarp(graph, source, sink);
+
+                std::cout << "Maximum flow: " << result->getMaxFlow() << std::endl;
+
+                std::cout << "Graph with flow: " << std::endl;
+                std::cout << result->getGraph()->ToString() << std::endl;
+                break;
+            }
+            case 2: {
+                std::cout << "Minimum cost flow: " << std::endl;
+                break;
+            }
+            case 3: {
+                return 0;
+            }
+            default: {
+                std::cout << "ERROR: Invalid choice" << std::endl;
+                return 1;
+            }
+        }
     } catch (std::invalid_argument& e) {
         std::cout << "ERROR: " << e.what() << std::endl;
         return -1;
